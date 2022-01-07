1a0b9b888736be636b89d48bf15fa881599a1fd0
暐柏 江 <108111134@mail.aeust.edu.tw>
2022-01-07 16:55:21 +0800

    4456465

--- 110_1Quiz4/Global.asax.cs
+++ 110_1Quiz4/Global.asax.cs
@@ -14,3 +14,21 @@ namespace _110_1Quiz4
         }
     }
 }
+
+
+<%@ Page Language = "C#" AutoEventWireup="true" CodeBehind="Quize4.aspx.cs" Inherits="_110_1Quiz4.Quize4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
